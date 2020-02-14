00e6c0e4d4b427f9940dc000bb88603f03d3796b
lznCesar <celori54@gmail.com>
2020-02-14 10:38:46 -0600

    modificacion con formula entre puntos

--- Practica 9 - distancia entre puntos A y B/js/app.js
+++ Practica 9 - distancia entre puntos A y B/js/app.js
@@ -1,14 +1,19 @@
-let puntoA, puntoB, DistanciaEntrePuntos;
+let puntoAx, puntoBx, puntoAy, puntoBy, resultadointerno, DistanciaEntrePuntos;
 
 let button = document.getElementById('botondistancia');
 button.addEventListener("click", calculardistancia);
 
 function calculardistancia(){
 
-    puntoA = Number(document.getElementById('PsnA').value);
-    puntoB = Number(document.getElementById('PsnB').value);
+    puntoAx = Number(document.getElementById('PsnAX').value);
+    puntoBx = Number(document.getElementById('PsnBX').value);
+    puntoAy = Number(document.getElementById('PsnAY').value);
+    puntoBy = Number(document.getElementById('PsnBY').value);
 
-    DistanciaEntrePuntos = puntoB - puntoA;
+    resultadointerno = Math.pow((puntoBx - puntoAx),2) + Math.pow((puntoBy -  puntoAy),2);
+    console.log(resultadointerno);
+
+    DistanciaEntrePuntos = Math.sqrt(resultadointerno);
 
     let imprimirdistancia = document.getElementById('imprimirdistancia');
     imprimirdistancia.innerText=DistanciaEntrePuntos;
