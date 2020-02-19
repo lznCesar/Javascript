68bf338d0bef8497400f9634da7b9a6642f53318
lznCesar <celori54@gmail.com>
2020-02-18 20:34:25 -0600

    if simples

---
+++ Con. 5 Clave del producto /js/app.js
@@ -0,0 +1,14 @@
+const button = document.getElementById('btnDetalles');
+button.addEventListener('click', DetallesProducto);
+
+function DetallesProducto(){
+
+    const Nombre = document.getElementById('nombreP').value;
+    const Clave = document.getElementById('ClaveP').value;
+    const Precio = document.getElementById('PrecioP').value;
+
+    const printInfo = document.getElementById('printInfo');
+    
+
+}
+
