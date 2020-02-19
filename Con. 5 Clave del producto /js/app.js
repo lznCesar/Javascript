de3bab8012f780c6559bbb4c4ca1b79991821aae
lznCesar <celori54@gmail.com>
2020-02-18 21:16:57 -0600

    realizamos pseudocodigo para obtener descuento en productos a travez de la clave del mismo

--- Con. 5 Clave del producto /js/app.js
+++ Con. 5 Clave del producto /js/app.js
@@ -1,14 +1,44 @@
-const button = document.getElementById('btnDetalles');
-button.addEventListener('click', DetallesProducto);
+const button = document.getElementById("btnDetalles");
+button.addEventListener("click", DetallesProducto);
 
-function DetallesProducto(){
+function DetallesProducto() {
+  const Nombre = document.getElementById("nombreP").value;
+  const Clave = document.getElementById("ClaveP").value;
+  const Precio = document.getElementById("PrecioP").value;
 
-    const Nombre = document.getElementById('nombreP').value;
-    const Clave = document.getElementById('ClaveP').value;
-    const Precio = document.getElementById('PrecioP').value;
+  const printInfo = document.getElementById("printInfo");
 
-    const printInfo = document.getElementById('printInfo');
-    
-
-}
+  if (Clave == 01) {
+    let descuento = (Precio * 10) / 100;
+    let PrecioDescuento = Precio - descuento;
+    printInfo.innerText =
+      "Nombre del articulo" +
+      Nombre +
+      "Clave del articulo:" +
+      Clave +
+      "Precio original:" +
+      Precio +
+      "Precio Con descuento" +
+      PrecioDescuento;
+  } else{
+      printInfo.innerText =  "Nombre del articulo" +
+      Nombre +
+      "Clave del articulo:" +
+      Clave +
+      "Precio original:" +
+      Precio;
+  } if (Clave == 02){
+    let descuento20 = (Precio * 20) / 100;
+    let PrecioDescuento20 = Precio - descuento20;
+    printInfo.innerText =
+      "Nombre del articulo" +
+      Nombre +
+      "Clave del articulo:" +
+      Clave +
+      "Precio original:" +
+      Precio +
+      "Precio Con descuento" +
+      PrecioDescuento20;
+  }
+  }
 
